480c04b87c812dcf15f0b3bb75669f9fb70c5c12
freshjustin <justin@abraham.email>
2018-06-01 22:44:43 -0500

    Changed to CURL_MULTI processing
    1. file_get_contents was removed and changed to curl_multi to allow for multiple urls to be handled all at once.
    2. to and from options was removed and new option workers was added
    3. workers is used to chunk handles in the sitemap automatically and then process each chunk via curl_multi_exec. For this reason since the sitemap will automatically get processed to and from options were removed.
    
    Options now available are:
    key
    url
    sleep
    workers

--- class/PHP_Warmer.class.php
+++ class/PHP_Warmer.class.php
@@ -11,7 +11,9 @@ class PHP_Warmer
     var $context;
     var $from;
     var $to;
+    var $workers;
     var $urlProblems = array();
+    var $_curl_headers = array();
 
     function __construct($config)
     {
@@ -24,6 +26,7 @@ class PHP_Warmer
         $this->sleep_time = (int)$this->get_parameter('sleep', 0);
         $this->from = (int)$this->get_parameter('from', 0);
         $this->to = (int)$this->get_parameter('to', false);
+	    $this->workers = (int)$this->get_parameter('workers', 2);
         $this->response = new PHP_Warmer_Response();
         $this->context = stream_context_create(
             /****
@@ -52,28 +55,77 @@ class PHP_Warmer
                 $timer = new PHP_Warmer_Timer();
                 $timer->start();
 
-                // Discover URL links
-                $urls = $this->process_sitemap($sitemap_url);
-                sort($urls);
-                
-                // Visit links
-                foreach($urls as $url)
-                {
-                    if($this->from <= $counter && 
-                            (empty($this->to) || (!empty($this->to) && $this->to > $counter) )) {
-                        $url_content = @file_get_contents($url,false,$this->context);
 
-                        // Prepare info about URLs with error
-                        if ($url_content === false && $this->config['reportProblematicUrls']) {
-                            $this->urlProblems[] = $url;
-                        }
+                $referer = null ;
+                if ( isset($_SERVER['HTTP_HOST']) && isset($_SERVER['REQUEST_URI']) ) {
+                    $referer = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ;
+                }
 
-                        if(($this->sleep_time > 0))
-                            sleep($this->sleep_time);
+                $options = array(
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_HEADER => true,
+                    CURLOPT_CUSTOMREQUEST => 'GET',
+                    CURLOPT_FOLLOWLOCATION => false,
+                    CURLOPT_ENCODING => 'gzip',
+                    CURLOPT_CONNECTTIMEOUT => 10,
+                    CURLOPT_TIMEOUT => 10,
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => false,
+                    CURLOPT_NOBODY => false,
+                    CURLOPT_HTTPHEADER => $this->_curl_headers,
+                ) ;
+                $options[CURLOPT_HTTPHEADER][] = "Cache-Control: max-age=0" ;
+                $options[ CURL_HTTP_VERSION_1_1 ] = 1 ;
+                $options[CURLOPT_USERAGENT] = 'lscache_runner';
+                $options[CURLOPT_HTTPHEADER][] = "Host: canadiancoupons.net";
+                if ( !empty($referer) ) {
+                    $options[CURLOPT_REFERER] = $referer ;
+                }
 
-                        $this->response->set_visited_url($url);
+	            // Discover URL links
+	            $urls = $this->process_sitemap($sitemap_url);
+	            sort($urls);
+	            $urlChunks = array_chunk($urls,$this->workers);
+
+	            // Visit links
+	            foreach ($urlChunks as $urls)
+	            {
+		            $mh = curl_multi_init() ;
+		            $ch = array() ;
+                    foreach ($urls as $i => $url) {
+	                    $ch[ $i ] = curl_init();
+	                    curl_setopt( $ch[ $i ], CURLOPT_URL, $url );
+	                    curl_setopt_array( $ch[ $i ], $options );
+	                    curl_multi_add_handle( $mh, $ch[ $i ] );
                     }
-                    $counter++;
+					$running = null;
+                    do {
+	                    $url_content = curl_multi_exec($mh, $running);
+	                    curl_multi_select($mh);
+                    } while ($running > 0);
+
+                    foreach ($urls as $i => $url) {
+	                    $thisCurl = $ch[$i] ;
+
+	                    curl_multi_remove_handle($mh, $thisCurl) ;
+	                    curl_close($thisCurl) ;
+                    }
+                    curl_multi_close($mh) ;
+
+                    $header['content'] = $url_content;
+                    //echo '<pre>', var_dump($header), '</pre>';
+
+                    // Prepare info about URLs with error
+                    if ( $url_content === false && $this->config['reportProblematicUrls'] ) {
+	                    $this->urlProblems[] = $urls;
+                    }
+
+                    if ( ( $this->sleep_time > 0 ) ) {
+	                    sleep( $this->sleep_time );
+                    }
+
+                    $this->response->set_visited_url( $urls );
+
                 }
 
                 //Stop timer
